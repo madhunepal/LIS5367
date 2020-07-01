225616b6b078235c54647cb46bc54b94de412708
madhunepal <madhunepal@gmail.com>
2020-06-30 23:26:53 -0400

    log out and dashboard codes added

--- loginfiles/logout.php
+++ loginfiles/logout.php
@@ -1,3 +1,14 @@
 <?php
-echo 'You are log out of the system now.'
+// Initialize the session
+session_start();
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
+session_destroy();
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
 ?>
